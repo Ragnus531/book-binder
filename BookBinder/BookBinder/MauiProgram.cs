abda2defce477ed8b91ac65f579db3591edd345b
Ragnus531 <jakub8327@gmail.com>
2024-04-15 22:12:12 +0200

    installin uranium material

--- BookBinder/BookBinder/MauiProgram.cs
+++ BookBinder/BookBinder/MauiProgram.cs
@@ -1,38 +1,42 @@
-﻿namespace BookBinder;
+﻿using UraniumUI;
+
+namespace BookBinder;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.UseMauiCommunityToolkit()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("FontAwesome6FreeBrands.otf", "FontAwesomeBrands");
-				fonts.AddFont("FontAwesome6FreeRegular.otf", "FontAwesomeRegular");
-				fonts.AddFont("FontAwesome6FreeSolid.otf", "FontAwesomeSolid");
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .UseUraniumUI()
+            .UseUraniumUIMaterial()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("FontAwesome6FreeBrands.otf", "FontAwesomeBrands");
+                fonts.AddFont("FontAwesome6FreeRegular.otf", "FontAwesomeRegular");
+                fonts.AddFont("FontAwesome6FreeSolid.otf", "FontAwesomeSolid");
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		builder.Services.AddSingleton<MainViewModel>();
+        builder.Services.AddSingleton<MainViewModel>();
 
-		builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddSingleton<MainPage>();
 
-		builder.Services.AddTransient<SampleDataService>();
-		builder.Services.AddTransient<ListDetailDetailViewModel>();
-		builder.Services.AddTransient<ListDetailDetailPage>();
+        builder.Services.AddTransient<SampleDataService>();
+        builder.Services.AddTransient<ListDetailDetailViewModel>();
+        builder.Services.AddTransient<ListDetailDetailPage>();
 
-		builder.Services.AddSingleton<ListDetailViewModel>();
+        builder.Services.AddSingleton<ListDetailViewModel>();
 
-		builder.Services.AddSingleton<ListDetailPage>();
+        builder.Services.AddSingleton<ListDetailPage>();
 
-		builder.Services.AddSingleton<LocalizationViewModel>();
+        builder.Services.AddSingleton<LocalizationViewModel>();
 
-		builder.Services.AddSingleton<LocalizationPage>();
+        builder.Services.AddSingleton<LocalizationPage>();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
