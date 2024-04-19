34231297d1ae4d000b35899e216b4ecf20e02467
Ragnus531 <jakub8327@gmail.com>
2024-04-19 22:22:42 +0200

    IDK

--- BookBinder/BookBinder/MauiProgram.cs
+++ BookBinder/BookBinder/MauiProgram.cs
@@ -14,11 +14,7 @@ public static class MauiProgram
             .UseUraniumUIMaterial()
             .ConfigureFonts(fonts =>
             {
-                fonts.AddFont("FontAwesome6FreeBrands.otf", "FontAwesomeBrands");
-                fonts.AddFont("FontAwesome6FreeRegular.otf", "FontAwesomeRegular");
                 fonts.AddFont("FontAwesome6FreeSolid.otf", "FontAwesomeSolid");
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
         builder.Services.AddSingleton<MainViewModel>();
