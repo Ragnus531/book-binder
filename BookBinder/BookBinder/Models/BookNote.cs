f98b79a73361858ec832f39dbeaf9056796da681
Ragnus531 <jakub8327@gmail.com>
2024-04-20 21:58:10 +0200

    Grouped collection

---
+++ BookBinder/BookBinder/Models/BookNote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBinder.Models
+{
+    public class BookNoteGroup : List<BookNote>
+    {
+        public string GroupTitle { get; set; }
+
+        public BookNoteGroup(string groupTitle, List<BookNote> bookNotes)
+            : base(bookNotes)
+        {
+            GroupTitle = groupTitle;
+        }
+    }
+
+    public class BookNote
+    {
+        public string Title { get; set; }
+        public string AbsolutePath { get; set; }
+    }
+}
--- BookBinder/BookBinder/ViewModels/MainViewModel.cs
+++ BookBinder/BookBinder/ViewModels/MainViewModel.cs
@@ -2,4 +2,50 @@
 
 public partial class MainViewModel : BaseViewModel
 {
+    public List<BookNoteGroup> BookNotes { get; set; } = new List<BookNoteGroup>();
+
+    public MainViewModel()
+    {
+        BookNotes.Add(
+            new BookNoteGroup(
+                "B",
+                new List<BookNote>
+                {
+                    new BookNote { Title = "Borgos" },
+                    new BookNote { Title = "Batman" },
+                }
+            )
+        );
+
+        BookNotes.Add(
+            new BookNoteGroup(
+                "D",
+                new List<BookNote>
+                {
+                    new BookNote { Title = "Diablo" },
+                    new BookNote { Title = "Diaman" },
+                }
+            )
+        );
+
+        BookNotes.Add(
+            new BookNoteGroup("H", new List<BookNote> { new BookNote { Title = "Hobbit" }, })
+        );
+
+        BookNotes.Add(
+            new BookNoteGroup("O", new List<BookNote> { new BookNote { Title = "Outpost" }, })
+        );
+
+        BookNotes.Add(
+            new BookNoteGroup(
+                "S",
+                new List<BookNote>
+                {
+                    new BookNote { Title = "Star wars" },
+                    new BookNote { Title = "Shrek" },
+                    new BookNote { Title = "Sekiro" }
+                }
+            )
+        );
+    }
 }
